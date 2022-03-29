3d972536e1c6867dbe3b04a84f7d46cf168efbc2
teoga <teoga@TeoPC>
2022-03-29 00:50:00 -0300

    update2

---
+++ AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/Contratos/Modelo/ModeloPrueba.java
@@ -0,0 +1,5 @@
+package CuartoMedio.LegislacionLaboral.Contratos.Modelo;
+
+public class ModeloPrueba {
+
+}
---
+++ AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/Contratos/Vista/VistaContratos.java
@@ -0,0 +1,46 @@
+package CuartoMedio.LegislacionLaboral.Contratos.Vista;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+
+public class VistaContratos extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VistaContratos frame = new VistaContratos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VistaContratos() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Contratos");
+		lblNewLabel.setBounds(136, 30, 158, 29);
+		contentPane.add(lblNewLabel);
+	}
+}
