194f44fddfd01ee167ecc80b5e6eea53e1fd2b5e
teoGarcia <teofilo.garcia04@gmail.com>
2022-03-29 22:07:01 -0300

    update2

--- AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/Contratos/Vista/VistaContratos.java
+++ AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/Contratos/Vista/VistaContratos.java
@@ -7,6 +7,11 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTabbedPane;
+import java.awt.Font;
+import javax.swing.JEditorPane;
+import javax.swing.JButton;
 
 public class VistaContratos extends JFrame {
 
@@ -33,14 +38,46 @@ public class VistaContratos extends JFrame {
 	 */
 	public VistaContratos() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 800, 768);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Contratos");
-		lblNewLabel.setBounds(136, 30, 158, 29);
+		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 18));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(0, 52, 784, 29);
 		contentPane.add(lblNewLabel);
+		
+		JTabbedPane tabbedPaneContratos = new JTabbedPane(JTabbedPane.TOP);
+		tabbedPaneContratos.setBounds(10, 181, 764, 418);
+		tabbedPaneContratos.setFont(new Font("Dialog", Font.BOLD, 14));
+		contentPane.add(tabbedPaneContratos);
+		
+		JPanel panel = new JPanel();
+		tabbedPaneContratos.addTab("Indefinido", null, panel, null);
+		panel.setLayout(null);
+		
+		JPanel panel_1 = new JPanel();
+		tabbedPaneContratos.addTab("Plazo fijo", null, panel_1, null);
+		
+		JPanel panel_2 = new JPanel();
+		tabbedPaneContratos.addTab("Honorarios", null, panel_2, null);
+		
+		JEditorPane editorPaneIndefinido = new JEditorPane();
+		editorPaneIndefinido.setBounds(23, 35, 711, 231);
+		panel.add(editorPaneIndefinido);
+		
+		JButton btnPracticarIndefinido = new JButton("Practicar");
+		btnPracticarIndefinido.setBounds(645, 321, 89, 35);
+		panel.add(btnPracticarIndefinido);
+		
+		JButton btnEjemploIndefinido = new JButton("Ejemplo");
+		btnEjemploIndefinido.setBounds(527, 321, 89, 35);
+		panel.add(btnEjemploIndefinido);
+		
+		
+		
 	}
 }
