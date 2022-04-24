359c53ca80ccd58e5b3cb99ab4e7c0d5b4a07b8d
jampoolG <jampoolgarcia@gmail.com>
2022-04-23 20:06:18 -0400

    agregada la clase main

---
+++ AppAdministracionEmpresas/src/Main/Main.java
@@ -0,0 +1,30 @@
+package Main;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+import Menu.VistaMenu;
+import Menus.MenuPrincipal.VistaMenuPrincipal;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JFrame window = Window.getInstance();
+					Window.registerPanel(new VistaMenu(), "menu");
+					window.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+
+}
