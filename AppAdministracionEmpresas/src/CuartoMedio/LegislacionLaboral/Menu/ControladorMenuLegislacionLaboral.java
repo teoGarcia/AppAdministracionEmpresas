76b9fa9157e11a15f61b1528866f6682c76c2feb
jampoolG <jampoolgarcia@gmail.com>
2022-04-23 20:17:16 -0400

    agregado el menu de legislacion laboral

---
+++ AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/Menu/ControladorMenuLegislacionLaboral.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package CuartoMedio.LegislacionLaboral.Menu;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import Menu.SideMenu;
+
+/**
+ * @author Dead
+ *
+ */
+public class ControladorMenuLegislacionLaboral implements ActionListener {
+
+	private VistaMenuLegislacionLaboral vmll;
+
+	public ControladorMenuLegislacionLaboral(VistaMenuLegislacionLaboral vmll) {
+		this.vmll = vmll;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String name = e.getActionCommand();
+		if(!SideMenu.isContentPanel(name)) lazinLoadView(e.getSource());
+		SideMenu.changeContentPanel(name);
+		
+	}
+
+	private void lazinLoadView(Object btn) {
+		// TODO Auto-generated method stub
+		if(btn.equals(vmll.getBtnFinTrab())) {
+			//SideMenu.registerContentPanel(new VistaMenuLegislacionLaboral(), vm4m.getBtnLegisLabo().getText());
+		}
+	}
+
+}
---
+++ AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/Menu/VistaMenuLegislacionLaboral.java
@@ -0,0 +1,79 @@
+package CuartoMedio.LegislacionLaboral.Menu;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+import javax.swing.border.LineBorder;
+
+import CuartoMedio.LegislacionLaboral.Contratos.Vista.VistaContratos;
+import Menu.SideMenu;
+
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VistaMenuLegislacionLaboral extends JPanel {
+	
+	private JButton btnConTra;
+	private JButton btnPerLab;
+	private JButton btnCerVac;
+	private JButton btnFinTrab;
+	private ControladorMenuLegislacionLaboral cmll;
+
+	/**
+	 * Create the panel.
+	 */
+	public VistaMenuLegislacionLaboral() {
+		inicialize();
+		contentViewDefault();
+	}
+
+	private void inicialize() {
+		
+		cmll = new ControladorMenuLegislacionLaboral(this);
+		
+		setBounds(0, 0, 208, 476);
+		setLayout(new GridLayout(0, 1, 0, 0));
+		setOpaque(false);
+		
+		btnConTra = new JButton("Contratos De Trabajo");
+		btnConTra.addActionListener(cmll);
+		btnConTra.setForeground(Color.WHITE);
+		btnConTra.setBorder(new LineBorder(new Color(0, 0, 0)));
+		btnConTra.setBackground(new Color(54, 54, 54));
+		btnConTra.setFont(new Font("Dialog", Font.PLAIN, 15));
+		add(btnConTra);
+		
+		btnPerLab = new JButton("Permisos Laborales");
+		btnPerLab.setForeground(Color.WHITE);
+		btnPerLab.setBorder(new LineBorder(new Color(0, 0, 0)));
+		btnPerLab.setFont(new Font("Dialog", Font.PLAIN, 15));
+		btnPerLab.setBackground(new Color(54, 54, 54));
+		add(btnPerLab);
+		
+		btnCerVac = new JButton("Certificado de vacaciones");
+		btnCerVac.setForeground(Color.WHITE);
+		btnCerVac.setBorder(new LineBorder(new Color(0, 0, 0)));
+		btnCerVac.setFont(new Font("Dialog", Font.PLAIN, 15));
+		btnCerVac.setBackground(new Color(54, 54, 54));
+		add(btnCerVac);
+		
+		btnFinTrab = new JButton("Finiquitos de trabajo");
+		btnFinTrab.setForeground(Color.WHITE);
+		btnFinTrab.setBorder(new LineBorder(new Color(0, 0, 0)));
+		btnFinTrab.setBackground(new Color(54, 54, 54));
+		btnFinTrab.setFont(new Font("Dialog", Font.PLAIN, 15));
+		add(btnFinTrab);
+		
+	}
+	
+	private void contentViewDefault() {
+		SideMenu.registerContentPanel(new VistaContratos(), btnConTra.getText());
+	}
+
+	public JButton getBtnFinTrab() {
+		return btnFinTrab;
+	}
+}
