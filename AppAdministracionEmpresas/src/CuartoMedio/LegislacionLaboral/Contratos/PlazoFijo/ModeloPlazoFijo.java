a575d216d59cfae57b075248373e6b0ee53eb8b9
jampoolG <jampoolgarcia@gmail.com>
2022-04-24 20:37:08 -0400

    creado modelo plazo fijo

--- AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/Contratos/PlazoFijo/ModeloPlazoFijo.java
+++ AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/Contratos/PlazoFijo/ModeloPlazoFijo.java
@@ -1,4 +1,4 @@
-package CuartoMedio.LegislacionLaboral.Contratos.Indefinido;
+package CuartoMedio.LegislacionLaboral.Contratos.PlazoFijo;
 
 import java.util.ArrayList;
 
@@ -6,18 +6,16 @@ import javax.swing.ImageIcon;
 
 import Helpers.ImageUrls;
 
-public class ModeloIndefinido {
+public class ModeloPlazoFijo {
 
-	private final String document = "MODELO DE CONTRATO PLAZO FIJO.doc";
+	private final String document = "MODELO DE CONTRATO DE TRABAJO INDEFINIDO 1.docx";
 	private ArrayList<ImageIcon> images = new ArrayList<ImageIcon>();
 	private String[] urls = {
-			"MODELO DE CONTRATO DE TRABAJO INDEFINIDO 1_00001.jpg", 
-			"MODELO DE CONTRATO DE TRABAJO INDEFINIDO 1_00002.jpg",
-			"MODELO DE CONTRATO DE TRABAJO INDEFINIDO 1_00003.jpg",
-			"MODELO DE CONTRATO DE TRABAJO INDEFINIDO 1_00004.jpg"	
+			"MODELO DE CONTRATO HONORARIO_00001.jpg",
+			"MODELO DE CONTRATO HONORARIO_00002.jpg"
 	};
 	
-	public ModeloIndefinido() {
+	public ModeloPlazoFijo() {
 		buildingImages();
 	}
 	
