5176806f4b14611e0ad251d8d03f8fa8ba8c158d
jampoolG <jampoolgarcia@gmail.com>
2022-05-05 06:41:31 -0400

    agregada algunas definiciones

--- AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/PermisosLaborales/VistaPermisosLaborales.java
+++ AppAdministracionEmpresas/src/CuartoMedio/LegislacionLaboral/PermisosLaborales/VistaPermisosLaborales.java
@@ -13,15 +13,17 @@ import javax.swing.border.LineBorder;
 import CuartoMedio.LegislacionLaboral.Contratos.Indefinido.VistaIndefinido;
 import CuartoMedio.LegislacionLaboral.PermisosLaborales.DiasHoras.VistaDiasHoras;
 import CuartoMedio.LegislacionLaboral.PermisosLaborales.Solicitud.VistaSolicitud;
+import java.awt.ComponentOrientation;
 
 public class VistaPermisosLaborales extends JPanel {
 
 	public VistaPermisosLaborales() {
+		setOpaque(false);
+		setBackground(Color.DARK_GRAY);
 		inicialize();
 	}
 	
 	private void inicialize() {
-		setOpaque(false);
 		setBounds(0, 0, 774, 731);
 		setLayout(null);
 		
@@ -35,9 +37,8 @@ public class VistaPermisosLaborales extends JPanel {
 		JEditorPane txtDefinicion = new JEditorPane();
 		txtDefinicion.setForeground(new Color(255, 255, 255));
 		txtDefinicion.setOpaque(false);
-		txtDefinicion.setEditable(false);
 		txtDefinicion.setFont(new Font("Dialog", Font.PLAIN, 14));
-		txtDefinicion.setText("DEFINICION");
+		txtDefinicion.setText("Una permiso laboral es b\u00E1sicamente una manera formal de solicitar un descanso del trabajo durante un per\u00EDodo de tiempo particular. El motivo debe incluirse en el permiso y es esencial para asegurarce de que el instituto u organizaci\u00F3n lo acepte. \r\nSi el motivo legal para la solicitud, no ser\u00E1 rechazada sin ninguna justificaci\u00F3n, especialmente cuando se deba a razones personales, como enfermedad, trabajo urgente o para visitar a padres enfermos, entre otros.");
 		txtDefinicion.setBackground(new Color(254, 254, 254));
 		txtDefinicion.setBounds(21, 95, 731, 99);
 		add(txtDefinicion);
@@ -52,10 +53,12 @@ public class VistaPermisosLaborales extends JPanel {
 		tabbedPaneContratos.setFont(new Font("Dialog", Font.BOLD, 14));
 		add(tabbedPaneContratos);
 		
-		VistaSolicitud vs = new VistaSolicitud("DEFINICION");
+		VistaSolicitud vs = new VistaSolicitud("Una solicitud de permiso laboral es una carta formal que se le escribe a el supervisor de la empresa para solicitar un tiempo fuera del trabajo. La carta formal es necesaria para su archivo personal. Proporciona un registro escrito de la notificaciónes que recibe y a la empresa antes de necesitar el permiso de ausencia. Si bien se puede explicar la situación en persona, una carta escrita proporciona la explicación exacta de la necesidad del tiempo libre. Esto hace que la solicitud sea más fácil de entender para el gerente o persona de recursos humanos.");
 		tabbedPaneContratos.addTab("Solicitud", null, vs, null);
 		
 		VistaDiasHoras vdh = new VistaDiasHoras("DEFINICION");
 		tabbedPaneContratos.addTab("Dias o Horas", null, vdh, null);
+		
+		
 	}
 }
