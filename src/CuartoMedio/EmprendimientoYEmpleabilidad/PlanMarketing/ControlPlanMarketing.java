b3e42ab3aabdb4c9a41913acffa5f9902fdc173a
teoGarcia <teofilo.garcia04@gmail.com>
2022-05-11 23:46:07 -0400

    Plan de Marketing

---
+++ src/CuartoMedio/EmprendimientoYEmpleabilidad/PlanMarketing/ControlPlanMarketing.java
@@ -0,0 +1,12 @@
+package CuartoMedio.EmprendimientoYEmpleabilidad.PlanMarketing;
+
+public class ControlPlanMarketing {
+	
+	private VistaPlanMarketing vpm;
+
+	public ControlPlanMarketing(VistaPlanMarketing vpm) {
+		this.vpm = vpm;
+		// TODO Auto-generated constructor stub
+	}
+
+}
