f17c5ddf6ea7585905a35f94efbc514e3107c6a0
jampoolG <jampoolgarcia@gmail.com>
2022-07-23 07:30:09 -0400

    creado y configurada la clase repository

---
+++ src/CuartoMedio/EmprendimientoYEmpleabilidad/PlanMarketing/PlanMarketingRepository.java
@@ -0,0 +1,7 @@
+package CuartoMedio.EmprendimientoYEmpleabilidad.PlanMarketing;
+
+import core.CrudRepositoryJPA;
+
+public class PlanMarketingRepository extends CrudRepositoryJPA<PlanMarketing, Long> {
+
+}
