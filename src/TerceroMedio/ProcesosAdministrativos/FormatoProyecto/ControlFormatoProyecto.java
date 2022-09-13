d00d845bae61a30830a323e6b7973cd7e6b940f2
teoGarcia <teofilo.garcia04@gmail.com>
2022-09-12 23:02:51 -0300

    1er modulo listo

--- src/TerceroMedio/ProcesosAdministrativos/FormatoProyecto/ControlFormatoProyecto.java
+++ src/TerceroMedio/ProcesosAdministrativos/FormatoProyecto/ControlFormatoProyecto.java
@@ -3,9 +3,19 @@ package TerceroMedio.ProcesosAdministrativos.FormatoProyecto;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
+import Helpers.AbrirDocumentos;
+import Helpers.AbrirDocumentos.Urls;
+import TerceroMedio.ProcesosAdministrativos.FormatoOrganigrama.ModeloOrganigrama;
+
 public class ControlFormatoProyecto implements ActionListener {
 	
 	private VistaFormatoProyecto vfp;
+	
+	private ModeloProyecto modelo = new ModeloProyecto();
+	private AbrirDocumentos mvd = AbrirDocumentos.getInstance();
+
 
 	public ControlFormatoProyecto(VistaFormatoProyecto vfp) {
 		this.vfp = vfp;
@@ -16,11 +26,11 @@ public class ControlFormatoProyecto implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		
 		if(e.getSource().equals(vfp.getBtnPreProNor())) {
-			
-			
+			JOptionPane.showMessageDialog(null, "AQUI PASO"+ modelo.getDocument());
+			mvd.VerDocumento(Urls.url3Doc, modelo.getDocument());
+		
 		}else if(e.getSource().equals(vfp.getBtnProInv())) {
-			
-			
+			mvd.VerDocumento(Urls.url3Doc, modelo.getDocument2());
 		}
 		
 	}
---
+++ src/TerceroMedio/ProcesosAdministrativos/FormatoProyecto/ModeloProyecto.java
@@ -0,0 +1,19 @@
+package TerceroMedio.ProcesosAdministrativos.FormatoProyecto;
+
+public class ModeloProyecto {
+	
+	private final String document = "PROYECTO NORMAL.doc";
+	private final String document2 = "PROYECTO DE INVESTIGACION.doc";
+	
+	
+
+	public String getDocument() {
+		return document;
+	}
+
+	public String getDocument2() {
+		return document2;
+	}
+
+
+}
