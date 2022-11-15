c920817aa0d17e96be3460f713a48f243b07ffc2
jampool garcia <jampoolgarcia@gmail.com>
2022-11-15 06:18:25 -0300

    creado balance general repository

---
+++ src/TerceroMedio/UtilizacionDeLaInformacionContable/BalanceGeneral/BalanceGeneralRepository.java
@@ -0,0 +1,7 @@
+package TerceroMedio.UtilizacionDeLaInformacionContable.BalanceGeneral;
+
+import core.CrudRepositoryJPA;
+
+public class BalanceGeneralRepository  extends CrudRepositoryJPA<BalanceGeneralEntity, Long>  {
+
+}
