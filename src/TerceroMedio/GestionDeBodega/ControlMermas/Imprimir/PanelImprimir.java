21322d6184ab604f0e35901148455aabc23096e6
JampoolG <Jampoolgarcia@gmail.com>
2022-12-04 08:10:51 -0300

    control de mermas de mercaderia

---
+++ src/TerceroMedio/GestionDeBodega/ControlMermas/Imprimir/PanelImprimir.java
@@ -0,0 +1,349 @@
+package TerceroMedio.GestionDeBodega.ControlMermas.Imprimir;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.print.PageFormat;
+import java.awt.print.Printable;
+import java.awt.print.PrinterException;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+
+import TerceroMedio.GestionDeBodega.ControlMermas.ControlMermaEntity;
+import core.Helpers;
+
+public class PanelImprimir extends JPanel implements Printable{
+	
+	private JLabel lblTMovimiento;
+	private JLabel lblMotivo;
+	private JLabel lblFolio;
+	private JLabel lblLocal;
+	private JLabel lblTiendaa;
+	private JLabel lblAlmacen;
+	private JLabel lblCentroCosto;
+	private JLabel lblFecha;
+	private JLabel lblSeccion;
+	private JLabel lblCodSapBarra;
+	private JLabel lblDescMaterial;
+	private JLabel lbUnidadMedica;
+	private JLabel lblCodigoMotivo;
+	private JLabel lblCantidad;
+
+	/**
+	 * Create the panel.
+	 */
+	public PanelImprimir() {
+		
+		setBackground(Color.WHITE);
+		
+		setBounds(0, 40, 584, 700);
+		setLayout(null);
+		
+		JLabel lblTitulo = new JLabel("Mermas y Consumos Autorizados");
+		lblTitulo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTitulo.setFont(new Font("Dialog", Font.BOLD, 15));
+		lblTitulo.setBounds(0, 80, 584, 33);
+		add(lblTitulo);
+		
+		JLabel lblProveedor_lbl = new JLabel("Tipo de Movimiento:");
+		lblProveedor_lbl.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblProveedor_lbl.setBounds(61, 151, 118, 22);
+		add(lblProveedor_lbl);
+		
+		lblTMovimiento = new JLabel("Tipo de movimiento");
+		lblTMovimiento.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblTMovimiento.setBounds(174, 151, 135, 22);
+		add(lblTMovimiento);
+		
+		JLabel lblDescripcionDeProducto = new JLabel("Motivo");
+		lblDescripcionDeProducto.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblDescripcionDeProducto.setBounds(319, 150, 75, 22);
+		add(lblDescripcionDeProducto);
+		
+		lblMotivo = new JLabel("motivo");
+		lblMotivo.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblMotivo.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblMotivo.setBounds(384, 151, 140, 22);
+		add(lblMotivo);
+		
+		JLabel lblCod = new JLabel("Folio:");
+		lblCod.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblCod.setBounds(60, 182, 102, 22);
+		add(lblCod);
+		
+		lblFolio = new JLabel("Folio");
+		lblFolio.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblFolio.setBounds(174, 183, 135, 22);
+		add(lblFolio);
+		
+		JLabel lblCodBarraEat = new JLabel("Local");
+		lblCodBarraEat.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblCodBarraEat.setBounds(319, 182, 75, 22);
+		add(lblCodBarraEat);
+		
+		lblLocal = new JLabel("Local");
+		lblLocal.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblLocal.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblLocal.setBounds(384, 182, 140, 22);
+		add(lblLocal);
+		
+		JLabel lblTienda = new JLabel("Tienda");
+		lblTienda.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblTienda.setBounds(61, 214, 102, 22);
+		add(lblTienda);
+		
+		lblTiendaa = new JLabel("Tienda");
+		lblTiendaa.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblTiendaa.setBounds(174, 214, 135, 22);
+		add(lblTiendaa);
+		
+		JLabel lblPaisDeAbasteciomiento = new JLabel("Almacen:");
+		lblPaisDeAbasteciomiento.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblPaisDeAbasteciomiento.setBounds(319, 213, 75, 22);
+		add(lblPaisDeAbasteciomiento);
+		
+		lblAlmacen = new JLabel("Almacen");
+		lblAlmacen.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblAlmacen.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblAlmacen.setBounds(384, 214, 140, 22);
+		add(lblAlmacen);
+		
+		JLabel lblPaisDeFabricacion = new JLabel("Centro costo:");
+		lblPaisDeFabricacion.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblPaisDeFabricacion.setBounds(61, 255, 101, 22);
+		add(lblPaisDeFabricacion);
+		
+		lblCentroCosto = new JLabel("Centro costo");
+		lblCentroCosto.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblCentroCosto.setBounds(174, 255, 135, 22);
+		add(lblCentroCosto);
+		
+		JLabel lblSeccion_lbl = new JLabel("Seccion:");
+		lblSeccion_lbl.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblSeccion_lbl.setBounds(319, 255, 75, 22);
+		add(lblSeccion_lbl);
+		
+		lblSeccion = new JLabel("seccion");
+		lblSeccion.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblSeccion.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblSeccion.setBounds(384, 255, 140, 22);
+		add(lblSeccion);
+		
+		JLabel lblPrecioSinImp = new JLabel("Fecha:");
+		lblPrecioSinImp.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblPrecioSinImp.setBounds(62, 344, 112, 22);
+		add(lblPrecioSinImp);
+		
+		lblFecha = new JLabel("fecha");
+		lblFecha.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblFecha.setBounds(172, 344, 118, 22);
+		add(lblFecha);
+		
+		JLabel lblCodBarra_lbl = new JLabel("Cod SAP / Barra:");
+		lblCodBarra_lbl.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblCodBarra_lbl.setBounds(319, 344, 90, 22);
+		add(lblCodBarra_lbl);
+		
+		lblCodSapBarra = new JLabel("Des per");
+		lblCodSapBarra.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblCodSapBarra.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblCodSapBarra.setBounds(406, 344, 118, 22);
+		add(lblCodSapBarra);
+		
+		JLabel lblDesc = new JLabel("Desc. Material:");
+		lblDesc.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblDesc.setBounds(61, 376, 112, 22);
+		add(lblDesc);
+		
+		lblDescMaterial = new JLabel("Desc. Material");
+		lblDescMaterial.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblDescMaterial.setBounds(172, 377, 118, 22);
+		add(lblDescMaterial);
+		
+		JLabel lblDescuentoIntro = new JLabel("Unidad Medica:");
+		lblDescuentoIntro.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblDescuentoIntro.setBounds(319, 376, 90, 22);
+		add(lblDescuentoIntro);
+		
+		lbUnidadMedica = new JLabel("Unidad Medica");
+		lbUnidadMedica.setHorizontalAlignment(SwingConstants.RIGHT);
+		lbUnidadMedica.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lbUnidadMedica.setBounds(418, 376, 106, 22);
+		add(lbUnidadMedica);
+		
+		JLabel lblIva_lbl = new JLabel("Codigo Motivo:");
+		lblIva_lbl.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblIva_lbl.setBounds(62, 408, 107, 22);
+		add(lblIva_lbl);
+		
+		lblCodigoMotivo = new JLabel("Codigo motivo");
+		lblCodigoMotivo.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblCodigoMotivo.setBounds(172, 408, 108, 22);
+		add(lblCodigoMotivo);
+		
+		JLabel lblcan = new JLabel("Cantidad:");
+		lblcan.setFont(new Font("Dialog", Font.BOLD, 10));
+		lblcan.setBounds(319, 407, 90, 22);
+		add(lblcan);
+		
+		lblCantidad = new JLabel("cantidad");
+		lblCantidad.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblCantidad.setFont(new Font("Dialog", Font.PLAIN, 10));
+		lblCantidad.setBounds(418, 408, 106, 22);
+		add(lblCantidad);
+		String[] columns = new String[] {"Id", "Descripcion", "Fecha", "Monto"};
+		
+	}
+	
+	public void cargarForm(ControlMermaEntity record, String TipoMovimiento, String Motivo, String UMedida) {
+		
+		lblTMovimiento.setText(TipoMovimiento);
+		lblMotivo.setText(Motivo);
+		lblFolio.setText(record.getFolio());
+		lblLocal.setText(record.getLocal());
+		lblTiendaa.setText(record.getTienda());
+		lblAlmacen.setText(record.getAlmacen());
+		lblCentroCosto.setText(record.getCentroCosto());
+		lblSeccion.setText(record.getSeccion());
+		
+		lblFecha.setText(Helpers.getFechaFormat(record.getFecha()));
+		lblCodSapBarra.setText(record.getCodSAPBARRAS());
+		lblDescMaterial.setText(record.getDescripcionMaterial());
+		lbUnidadMedica.setText(UMedida);
+		lblCodigoMotivo.setText(record.getCodigoMotivo());
+		lblCantidad.setText(record.getCantidad());
+		
+	}
+	
+	
+	public int print(Graphics graphics, PageFormat pageFormat, int pageIndex) throws PrinterException {
+		if(pageIndex == 0) {
+			Graphics2D graphics2D = (Graphics2D) graphics;
+			graphics2D.translate(pageFormat.getImageableX(), pageFormat.getImageableY());
+			printAll(graphics2D);
+			return PAGE_EXISTS;	
+			
+		}else {
+			return NO_SUCH_PAGE;
+			
+		}
+
+	}
+
+	public JLabel getLblProveedor() {
+		return lblTMovimiento;
+	}
+
+	public void setLblProveedor(JLabel lblProveedor) {
+		this.lblTMovimiento = lblProveedor;
+	}
+
+	public JLabel getLblProducto() {
+		return lblMotivo;
+	}
+
+	public void setLblProducto(JLabel lblProducto) {
+		this.lblMotivo = lblProducto;
+	}
+
+	public JLabel getLblCodIntProducto() {
+		return lblFolio;
+	}
+
+	public void setLblCodIntProducto(JLabel lblCodIntProducto) {
+		this.lblFolio = lblCodIntProducto;
+	}
+
+	public JLabel getLblCodBarraEAN13() {
+		return lblLocal;
+	}
+
+	public void setLblCodBarraEAN13(JLabel lblCodBarraEAN13) {
+		this.lblLocal = lblCodBarraEAN13;
+	}
+
+	public JLabel getLblCodBarraEAN14() {
+		return lblTiendaa;
+	}
+
+	public void setLblCodBarraEAN14(JLabel lblCodBarraEAN14) {
+		this.lblTiendaa = lblCodBarraEAN14;
+	}
+
+	public JLabel getLblPaisAbas() {
+		return lblAlmacen;
+	}
+
+	public void setLblPaisAbas(JLabel lblPaisAbas) {
+		this.lblAlmacen = lblPaisAbas;
+	}
+
+	public JLabel getLblPaisFrabri() {
+		return lblCentroCosto;
+	}
+
+	public void setLblPaisFrabri(JLabel lblPaisFrabri) {
+		this.lblCentroCosto = lblPaisFrabri;
+	}
+
+	public JLabel getLblPreSinImp() {
+		return lblFecha;
+	}
+
+	public void setLblPreSinImp(JLabel lblPreSinImp) {
+		this.lblFecha = lblPreSinImp;
+	}
+
+	public JLabel getLblMarca() {
+		return lblSeccion;
+	}
+
+	public void setLblMarca(JLabel lblMarca) {
+		this.lblSeccion = lblMarca;
+	}
+
+	public JLabel getLblDescPer() {
+		return lblCodSapBarra;
+	}
+
+	public void setLblDescPer(JLabel lblDescPer) {
+		this.lblCodSapBarra = lblDescPer;
+	}
+
+	public JLabel getLblPrecNeto() {
+		return lblDescMaterial;
+	}
+
+	public void setLblPrecNeto(JLabel lblPrecNeto) {
+		this.lblDescMaterial = lblPrecNeto;
+	}
+
+	public JLabel getLblDescIntro() {
+		return lbUnidadMedica;
+	}
+
+	public void setLblDescIntro(JLabel lblDescIntro) {
+		this.lbUnidadMedica = lblDescIntro;
+	}
+
+	public JLabel getLblIva() {
+		return lblCodigoMotivo;
+	}
+
+	public void setLblIva(JLabel lblIva) {
+		this.lblCodigoMotivo = lblIva;
+	}
+
+	public JLabel getLblPvp() {
+		return lblCantidad;
+	}
+
+	public void setLblPvp(JLabel lblPvp) {
+		this.lblCantidad = lblPvp;
+	}
+
+
+
+}
---
+++ src/TerceroMedio/GestionDeBodega/ControlMermas/VistaControlMerma.java
@@ -0,0 +1,494 @@
+package TerceroMedio.GestionDeBodega.ControlMermas;
+
+import javax.swing.JPanel;
+import ui.Labels.LabelTitulos;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+import ui.Labels.LabelSubtitulos;
+import javax.swing.JComboBox;
+import javax.swing.DefaultComboBoxModel;
+import ui.Texts.TextSoloNumeros;
+import javax.swing.JTextField;
+import com.toedter.calendar.JDateChooser;
+
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import ui.TablaUi.TableStandard;
+import ui.Buttons.StandarButton;
+
+public class VistaControlMerma extends JPanel {
+	
+	private JTextField txtLocal;
+	private JTextField txtTienda;
+	private JTextField txtAlmacen;
+	private JTextField txtCentroCosto;
+	private JTextField txtSeccion;
+	private JTextField txtCodSapBarras;
+	private JTextField txtDescripcionMaterial;
+	private JTextField txtMotivo;
+	private TableStandard table;
+	private ControladorControlMerma cmm;
+	private StandarButton btnModificar;
+	private StandarButton btnEliminar;
+	private StandarButton btnGuardar;
+	private JTextField txtId;
+	private TextSoloNumeros txtFolio;
+	private JDateChooser Fecha;
+	private TextSoloNumeros txtCantidad;
+	private JComboBox comboBoxMotivo;
+	private JComboBox comboBoxTMov;
+	private JComboBox comboBoxUnidadMedida;
+	private StandarButton btnImprimir;
+
+	/**
+	 * Create the panel.
+	 */
+	public VistaControlMerma() {
+		
+		cmm = new ControladorControlMerma(this);
+		
+		setBounds(0, 0, 774, 722);
+		setOpaque(false);
+		setLayout(null);
+		
+		LabelTitulos lbltlsMermasYConsumos = new LabelTitulos((String) null);
+		lbltlsMermasYConsumos.setHorizontalAlignment(SwingConstants.CENTER);
+		lbltlsMermasYConsumos.setText("Mermas y Consumos Autorizados");
+		lbltlsMermasYConsumos.setBounds(0, 60, 774, 30);
+		add(lbltlsMermasYConsumos);
+		
+		LabelSubtitulos lblsbtlsTipoDeMovimientos = new LabelSubtitulos((String) null);
+		lblsbtlsTipoDeMovimientos.setText("Tipo de Movimiento");
+		lblsbtlsTipoDeMovimientos.setBounds(34, 118, 132, 23);
+		add(lblsbtlsTipoDeMovimientos);
+		
+		comboBoxTMov = new JComboBox();
+		comboBoxTMov.setModel(new DefaultComboBoxModel(new String[] {"Consumo", "Merma"}));
+		comboBoxTMov.setBounds(145, 118, 137, 23);
+		add(comboBoxTMov);
+		
+		LabelSubtitulos lblsbtlsMotivo = new LabelSubtitulos((String) null);
+		lblsbtlsMotivo.setText("Motivo");
+		lblsbtlsMotivo.setBounds(284, 119, 104, 23);
+		add(lblsbtlsMotivo);
+		
+		comboBoxMotivo = new JComboBox();
+		comboBoxMotivo.setModel(new DefaultComboBoxModel(new String[] {"21.- Casino", "22.- Aseo", "23.- Operacion", "51.- Consumo", "53.- Vencimiento", "54.- Operacion"}));
+		comboBoxMotivo.setBounds(380, 118, 137, 23);
+		add(comboBoxMotivo);
+		
+		LabelSubtitulos lblsbtlsFolio = new LabelSubtitulos((String) null);
+		lblsbtlsFolio.setText("Folio");
+		lblsbtlsFolio.setBounds(521, 118, 64, 23);
+		add(lblsbtlsFolio);
+		
+		txtFolio = new TextSoloNumeros();
+		txtFolio.setBounds(595, 117, 137, 23);
+		add(txtFolio);
+		
+		LabelSubtitulos lblsbtlsLocal = new LabelSubtitulos((String) null);
+		lblsbtlsLocal.setText("Local");
+		lblsbtlsLocal.setBounds(34, 153, 132, 23);
+		add(lblsbtlsLocal);
+		
+		txtLocal = new JTextField();
+		txtLocal.setBounds(145, 153, 137, 23);
+		add(txtLocal);
+		txtLocal.setColumns(10);
+		
+		LabelSubtitulos lblsbtlsTienda = new LabelSubtitulos((String) null);
+		lblsbtlsTienda.setText("Tienda");
+		lblsbtlsTienda.setBounds(284, 153, 132, 23);
+		add(lblsbtlsTienda);
+		
+		txtTienda = new JTextField();
+		txtTienda.setColumns(10);
+		txtTienda.setBounds(380, 153, 137, 23);
+		add(txtTienda);
+		
+		LabelSubtitulos lblsbtlsAlmacen = new LabelSubtitulos((String) null);
+		lblsbtlsAlmacen.setText("Almacen");
+		lblsbtlsAlmacen.setBounds(521, 152, 89, 23);
+		add(lblsbtlsAlmacen);
+		
+		txtAlmacen = new JTextField();
+		txtAlmacen.setColumns(10);
+		txtAlmacen.setBounds(595, 153, 137, 23);
+		add(txtAlmacen);
+		
+		LabelSubtitulos lblsbtlsCentroCosto = new LabelSubtitulos((String) null);
+		lblsbtlsCentroCosto.setText("Centro Costo");
+		lblsbtlsCentroCosto.setBounds(34, 188, 132, 23);
+		add(lblsbtlsCentroCosto);
+		
+		txtCentroCosto = new JTextField();
+		txtCentroCosto.setColumns(10);
+		txtCentroCosto.setBounds(145, 188, 137, 23);
+		add(txtCentroCosto);
+		
+		LabelSubtitulos lblsbtlsSeccion = new LabelSubtitulos((String) null);
+		lblsbtlsSeccion.setText("Seccion");
+		lblsbtlsSeccion.setBounds(284, 188, 132, 23);
+		add(lblsbtlsSeccion);
+		
+		txtSeccion = new JTextField();
+		txtSeccion.setColumns(10);
+		txtSeccion.setBounds(380, 188, 137, 23);
+		add(txtSeccion);
+		
+		LabelSubtitulos lblsbtlsFecha = new LabelSubtitulos((String) null);
+		lblsbtlsFecha.setText("Fecha");
+		lblsbtlsFecha.setBounds(521, 187, 89, 23);
+		add(lblsbtlsFecha);
+		
+		Fecha = new JDateChooser();
+		Fecha.setBounds(595, 188, 137, 23);
+		add(Fecha);
+		
+		LabelSubtitulos lblsbtlsCodSap = new LabelSubtitulos((String) null);
+		lblsbtlsCodSap.setText("Cod SAP / Barras");
+		lblsbtlsCodSap.setBounds(34, 223, 132, 23);
+		add(lblsbtlsCodSap);
+		
+		txtCodSapBarras = new JTextField();
+		txtCodSapBarras.setColumns(10);
+		txtCodSapBarras.setBounds(145, 223, 137, 23);
+		add(txtCodSapBarras);
+		
+		LabelSubtitulos lblsbtlsDescMaterial = new LabelSubtitulos((String) null);
+		lblsbtlsDescMaterial.setText("Desc. Material");
+		lblsbtlsDescMaterial.setBounds(284, 223, 132, 23);
+		add(lblsbtlsDescMaterial);
+		
+		txtDescripcionMaterial = new JTextField();
+		txtDescripcionMaterial.setColumns(10);
+		txtDescripcionMaterial.setBounds(380, 223, 137, 23);
+		add(txtDescripcionMaterial);
+		
+		LabelSubtitulos lblsbtlsUnidadMedida = new LabelSubtitulos((String) null);
+		lblsbtlsUnidadMedida.setText("Unidad Med.");
+		lblsbtlsUnidadMedida.setBounds(521, 222, 89, 23);
+		add(lblsbtlsUnidadMedida);
+		
+		comboBoxUnidadMedida = new JComboBox();
+		comboBoxUnidadMedida.setModel(new DefaultComboBoxModel(new String[] {"C/U", "Pack", "Pallet", "Caja", "Kilo"}));
+		comboBoxUnidadMedida.setBounds(595, 223, 137, 23);
+		add(comboBoxUnidadMedida);
+		
+		LabelSubtitulos lblsbtlsCodMotivo = new LabelSubtitulos((String) null);
+		lblsbtlsCodMotivo.setText("Codigo Motivo");
+		lblsbtlsCodMotivo.setBounds(34, 260, 132, 23);
+		add(lblsbtlsCodMotivo);
+		
+		txtMotivo = new JTextField();
+		txtMotivo.setColumns(10);
+		txtMotivo.setBounds(145, 260, 137, 23);
+		add(txtMotivo);
+		
+		LabelSubtitulos lblsbtlsCantidad = new LabelSubtitulos((String) null);
+		lblsbtlsCantidad.setText("Cantidad");
+		lblsbtlsCantidad.setBounds(284, 260, 132, 23);
+		add(lblsbtlsCantidad);
+		
+		txtCantidad = new TextSoloNumeros();
+		txtCantidad.setBounds(380, 260, 137, 23);
+		add(txtCantidad);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(34, 294, 700, 343);
+		add(scrollPane);
+		
+		table = new TableStandard();
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		String[] columns = new String[] {"Id", "Cod. SAP/BARRAS", "Descripcion de Material", "Motivo", "U.M.", "Cantidad", "Tipo Movimiento", "Folio", "C. Costo", "Fecha", "Seccion", "Local", "Tienda", "Almacen"};
+		table.setColums(columns);
+		table.getColumnModel().getColumn(0).setPreferredWidth(40);
+		table.getColumnModel().getColumn(1).setPreferredWidth(120);
+		table.getColumnModel().getColumn(2).setPreferredWidth(200);
+		table.getColumnModel().getColumn(3).setPreferredWidth(120);
+		table.getColumnModel().getColumn(4).setPreferredWidth(80);
+		table.getColumnModel().getColumn(5).setPreferredWidth(120);
+		table.getColumnModel().getColumn(6).setPreferredWidth(120);
+		table.getColumnModel().getColumn(7).setPreferredWidth(120);
+		table.getColumnModel().getColumn(8).setPreferredWidth(120);
+		table.getColumnModel().getColumn(9).setPreferredWidth(120);
+		table.getColumnModel().getColumn(10).setPreferredWidth(120);
+		table.getColumnModel().getColumn(11).setPreferredWidth(120);
+		table.getColumnModel().getColumn(12).setPreferredWidth(120);
+		table.getColumnModel().getColumn(13).setPreferredWidth(120);
+		scrollPane.setViewportView(table);
+		
+		btnGuardar = new StandarButton((String) null);
+		btnGuardar.setText("Guardar");
+		btnGuardar.setBounds(628, 260, 104, 23);
+		btnGuardar.addActionListener(cmm);
+		add(btnGuardar);
+		
+		btnModificar = new StandarButton((String) null);
+		btnModificar.setText("Modificar");
+		btnModificar.setBounds(34, 648, 125, 30);
+		btnModificar.addActionListener(cmm);
+		add(btnModificar);
+		
+		btnEliminar = new StandarButton((String) null);
+		btnEliminar.setText("Eliminar");
+		btnEliminar.setBounds(169, 648, 125, 30);
+		btnEliminar.addActionListener(cmm);
+		add(btnEliminar);
+		
+		txtId = new JTextField();
+		txtId.setBounds(34, 25, 86, 20);
+		add(txtId);
+		txtId.setColumns(10);
+		
+		btnImprimir = new StandarButton((String) null);
+		btnImprimir.setText("Imprimir");
+		btnImprimir.setBounds(304, 648, 125, 30);
+		btnImprimir.addActionListener(cmm);
+		add(btnImprimir);
+		txtId.setVisible(false);
+		
+		ActualizarVista();
+
+	}
+	
+	public void VaciarForm() {
+		
+		comboBoxTMov.setSelectedIndex(0);
+		comboBoxMotivo.setSelectedIndex(0);
+		txtFolio.setText("");
+		txtLocal.setText("");
+		txtTienda.setText("");
+		txtAlmacen.setText("");
+		txtCentroCosto.setText("");
+		txtSeccion.setText("");
+		Fecha.setCalendar(null);
+		txtCodSapBarras.setText("");
+		txtDescripcionMaterial.setText("");
+		comboBoxUnidadMedida.setSelectedIndex(0);
+		txtMotivo.setText("");
+		txtCantidad.setText("");
+		txtId.setText("");
+		
+		
+	}
+	
+	public void ActualizarVista() {
+		VaciarForm();
+		cmm.LlenarTabla();
+		
+	}
+	
+	public void CargarForm(ControlMermaEntity mme, int indexTMov, int indexMotivo, int indexUM) {
+		
+		comboBoxTMov.setSelectedIndex(indexTMov);
+		comboBoxMotivo.setSelectedIndex(indexMotivo);
+		txtFolio.setText(mme.getFolio());
+		txtLocal.setText(mme.getLocal());
+		txtTienda.setText(mme.getTienda());
+		txtAlmacen.setText(mme.getAlmacen());
+		txtCentroCosto.setText(mme.getCentroCosto());
+		txtSeccion.setText(mme.getSeccion());
+		Fecha.setCalendar(mme.getFecha());
+		txtCodSapBarras.setText(mme.getCodSAPBARRAS());
+		txtDescripcionMaterial.setText(mme.getDescripcionMaterial());
+		comboBoxUnidadMedida.setSelectedIndex(indexUM);
+		txtMotivo.setText(mme.getCodigoMotivo());
+		txtCantidad.setText(mme.getCantidad());
+		txtId.setText(""+mme.getId());
+	}
+	
+	public boolean camposVacios() {
+		
+		if(txtFolio.getText().length() <= 0) {
+			return false;
+		}else if(txtLocal.getText().length() <= 0) {
+			return false;
+		}else if(txtTienda.getText().length() <= 0) {
+			return false;
+		}else if(txtAlmacen.getText().length() <= 0) {
+			return false;
+		}else if(txtCentroCosto.getText().length() <= 0) {
+			return false;
+		}else if(txtSeccion.getText().length() <= 0) {
+			return false;
+		}else if(Fecha.getCalendar() == null) {
+			return false;
+		}else if(txtCodSapBarras.getText().length() <= 0) {
+			return false;
+		}else if(txtDescripcionMaterial.getText().length() <= 0) {
+			return false;
+		}else if(txtMotivo.getText().length() <= 0) {
+			return false;
+		}else if(txtCantidad.getText().length() <= 0) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	public DefaultTableModel getModel() {
+		return table.getModel();
+	}
+
+	public StandarButton getBtnModificar() {
+		return btnModificar;
+	}
+
+	public StandarButton getBtnEliminar() {
+		return btnEliminar;
+	}
+
+	public StandarButton getBtnGuardar() {
+		return btnGuardar;
+	}
+
+	public void setBtnModificar(StandarButton btnModificar) {
+		this.btnModificar = btnModificar;
+	}
+
+	public void setBtnEliminar(StandarButton btnEliminar) {
+		this.btnEliminar = btnEliminar;
+	}
+
+	public void setBtnGuardar(StandarButton btnGuardar) {
+		this.btnGuardar = btnGuardar;
+	}
+
+	public JTextField getTxtLocal() {
+		return txtLocal;
+	}
+
+	public JTextField getTxtTienda() {
+		return txtTienda;
+	}
+
+	public JTextField getTxtAlmacen() {
+		return txtAlmacen;
+	}
+
+	public JTextField getTxtCentroCosto() {
+		return txtCentroCosto;
+	}
+
+	public JTextField getTxtSeccion() {
+		return txtSeccion;
+	}
+
+	public JTextField getTxtCodSapBarras() {
+		return txtCodSapBarras;
+	}
+
+	public JTextField getTxtDescripcionMaterial() {
+		return txtDescripcionMaterial;
+	}
+
+	public JTextField getTxtMotivo() {
+		return txtMotivo;
+	}
+
+	public JTextField getTxtId() {
+		return txtId;
+	}
+
+	public void setTxtLocal(JTextField txtLocal) {
+		this.txtLocal = txtLocal;
+	}
+
+	public void setTxtTienda(JTextField txtTienda) {
+		this.txtTienda = txtTienda;
+	}
+
+	public void setTxtAlmacen(JTextField txtAlmacen) {
+		this.txtAlmacen = txtAlmacen;
+	}
+
+	public void setTxtCentroCosto(JTextField txtCentroCosto) {
+		this.txtCentroCosto = txtCentroCosto;
+	}
+
+	public void setTxtSeccion(JTextField txtSeccion) {
+		this.txtSeccion = txtSeccion;
+	}
+
+	public void setTxtCodSapBarras(JTextField txtCodSapBarras) {
+		this.txtCodSapBarras = txtCodSapBarras;
+	}
+
+	public void setTxtDescripcionMaterial(JTextField txtDescripcionMaterial) {
+		this.txtDescripcionMaterial = txtDescripcionMaterial;
+	}
+
+	public void setTxtMotivo(JTextField txtMotivo) {
+		this.txtMotivo = txtMotivo;
+	}
+
+	public void setTxtId(JTextField txtId) {
+		this.txtId = txtId;
+	}
+
+	public TextSoloNumeros getTxtFolio() {
+		return txtFolio;
+	}
+
+	public void setTxtFolio(TextSoloNumeros txtFolio) {
+		this.txtFolio = txtFolio;
+	}
+
+	public TableStandard getTable() {
+		return table;
+	}
+
+	public void setTable(TableStandard table) {
+		this.table = table;
+	}
+
+	public JDateChooser getFecha() {
+		return Fecha;
+	}
+
+	public void setFecha(JDateChooser fecha) {
+		Fecha = fecha;
+	}
+
+	public TextSoloNumeros getTxtCantidad() {
+		return txtCantidad;
+	}
+
+	public void setTxtCantidad(TextSoloNumeros txtCantidad) {
+		this.txtCantidad = txtCantidad;
+	}
+
+	public JComboBox getComboBoxMotivo() {
+		return comboBoxMotivo;
+	}
+
+	public JComboBox getComboBoxTMov() {
+		return comboBoxTMov;
+	}
+
+	public JComboBox getComboBoxUnidadMedida() {
+		return comboBoxUnidadMedida;
+	}
+
+	public void setComboBoxMotivo(JComboBox comboBoxMotivo) {
+		this.comboBoxMotivo = comboBoxMotivo;
+	}
+
+	public void setComboBoxTMov(JComboBox comboBoxTMov) {
+		this.comboBoxTMov = comboBoxTMov;
+	}
+
+	public void setComboBoxUnidadMedida(JComboBox comboBoxUnidadMedida) {
+		this.comboBoxUnidadMedida = comboBoxUnidadMedida;
+	}
+
+	public StandarButton getBtnImprimir() {
+		return btnImprimir;
+	}
+
+	public void setBtnImprimir(StandarButton btnImprimir) {
+		this.btnImprimir = btnImprimir;
+	}
+}
