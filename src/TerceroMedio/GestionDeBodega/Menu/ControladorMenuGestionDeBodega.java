e44dec4ac9481cbbe52f9abc59934ed236b191fc
JampoolG <Jampoolgarcia@gmail.com>
2022-12-04 08:16:54 -0300

    agregado el menu de gestion de bodega

---
+++ src/TerceroMedio/GestionDeBodega/Menu/ControladorMenuGestionDeBodega.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package TerceroMedio.GestionDeBodega.Menu;
+
+import java.awt.event.ActionEvent;
+
+import java.awt.event.ActionListener;
+
+import Menu.Side.SideMenu;
+import TerceroMedio.GestionDeBodega.ControlIngresosEgresos.VistaControlIngresosYEgresos;
+import TerceroMedio.GestionDeBodega.ControlInventario.VistaControlInventario;
+import TerceroMedio.GestionDeBodega.ControlMermas.VistaControlMerma;
+import TerceroMedio.GestionDeBodega.FormatoProducto.VistaFormatoProductos;
+
+/**
+ * @author Dead
+ *
+ */
+public class ControladorMenuGestionDeBodega implements ActionListener {
+
+	private VistaMenuGestionBodega vmgb;
+
+	public ControladorMenuGestionDeBodega(VistaMenuGestionBodega vmgb) {
+		this.vmgb = vmgb;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String name = e.getActionCommand();
+		if(!SideMenu.isContentPanel(name)) lazinLoadView(e.getSource());
+		SideMenu.changeContentPanel(name);
+	}
+
+	private void lazinLoadView(Object btn) {
+		// TODO Auto-generated method stub
+		
+		if(btn.equals(vmgb.getBtnInventario())) {
+			SideMenu.registerContentPanel(new VistaControlInventario(), vmgb.getBtnInventario().getText()); 
+		}else if(btn.equals(vmgb.getBtnCodPro())) {
+			SideMenu.registerContentPanel(new VistaFormatoProductos(), vmgb.getBtnCodPro().getText()); 
+		}else if(btn.equals(vmgb.getBtnIngMer())) {
+			SideMenu.registerContentPanel(new VistaControlIngresosYEgresos(), vmgb.getBtnIngMer().getText()); 
+		}else if(btn.equals(vmgb.getBtnMerMer())) {
+			SideMenu.registerContentPanel(new VistaControlMerma(), vmgb.getBtnMerMer().getText()); 
+		}
+		 
+	}
+
+}
