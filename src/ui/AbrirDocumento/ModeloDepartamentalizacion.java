abb67e716c3147cd06b99e4f92ab57680b05ec00
JampoolG <Jampoolgarcia@gmail.com>
2022-12-08 22:55:20 -0300

    agregado ui documento

---
+++ src/ui/AbrirDocumento/ModeloDepartamentalizacion.java
@@ -0,0 +1,11 @@
+package ui.AbrirDocumento;
+
+public class ModeloDepartamentalizacion {
+	
+	private final String document = "MODELOS DE DEPARTAMENTALIZACION.docx";
+	
+	public String getDocument() {
+		return document;
+	}
+
+}
