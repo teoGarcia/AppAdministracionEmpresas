fa397dc972d42c044c8761d65c642f53a3015339
JampoolG <Jampoolgarcia@gmail.com>
2022-12-10 19:23:51 -0300

    imprimir depreciacion

--- src/TerceroMedio/UtilizacionDeLaInformacionContable/Deprecacion/ControlDeprecacion.java
+++ src/TerceroMedio/UtilizacionDeLaInformacionContable/Deprecacion/ControlDeprecacion.java
@@ -5,13 +5,16 @@ import java.awt.event.ActionListener;
 import java.util.Iterator;
 
 import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 
+import TerceroMedio.UtilizacionDeLaInformacionContable.Deprecacion.Imprimir.PanelDepreciacion;
 import TerceroMedio.UtilizacionDeLaInformacionContable.LibroDiarioYMayor.Asientos;
 import TerceroMedio.UtilizacionDeLaInformacionContable.LibroDiarioYMayor.AsientosRepository;
 import TerceroMedio.UtilizacionDeLaInformacionContable.LibroDiarioYMayor.LibroDiario;
 import core.Helpers;
 import core.ManagerDB;
 import ui.Mensejes.Mensajes;
+import ui.imprimir.VistaImprimir;
 
 public class ControlDeprecacion implements ActionListener {
 	
@@ -19,6 +22,8 @@ public class ControlDeprecacion implements ActionListener {
 	
 	private VistaDeprecacion vista;
 	private DeprecacionRepository Repository;
+	
+	private VistaImprimir imprimir;
 
 	public ControlDeprecacion(VistaDeprecacion vista) {
 		
@@ -76,40 +81,64 @@ public class ControlDeprecacion implements ActionListener {
 			}
 		} else if(e.getSource().equals(vista.getBtnCalcular())) {
 			
-			int valorInicial = Integer.parseInt(vista.getCrudDepreciacion().getSeledRow(1)); 
-			int valorRescate = Integer.parseInt(vista.getCrudDepreciacion().getSeledRow(2));
-			int vidaUtil = Integer.parseInt(vista.getCrudDepreciacion().getSeledRow(3));
+			CalcularDepreciacion(this.vista.getModelRecta(), this.vista.getModelAcelerado());
+				
+		} else if(e.getSource().equals(vista.getCrudDepreciacion().getBtnImprimir())) {
 			
-
-				int[][] dataLineaRecta = calcularDepreciacionLineaRecta(valorInicial, valorRescate, vidaUtil);
-				int[][] dataAcelerado = calcularDepreciacionAcelerada(valorInicial, valorRescate, vidaUtil);
-				
-				vista.clearTables();
-				
-				
-				for(int i = 0; i<vidaUtil; i++) {
-					this.vista.getModelRecta().addRow(new  Object[] {
-							dataLineaRecta[i][0],
-							dataLineaRecta[i][1],
-							dataLineaRecta[i][2]+"%",
-							dataLineaRecta[i][3],
-							dataLineaRecta[i][4],
-							dataLineaRecta[i][5],
-					});
-					
-					this.vista.getModelAcelerado().addRow(new  Object[] {
-							dataAcelerado[i][0],
-							dataAcelerado[i][1],
-							dataAcelerado[i][2]+"%",
-							dataAcelerado[i][3],
-							dataAcelerado[i][4],
-							dataAcelerado[i][5],
-					});
-					
-				}
+			if(imprimir == null) imprimir = VistaImprimir.instance();
+			imprimir.resetImprimir();
 			
-				
+			PanelDepreciacion pi = new PanelDepreciacion();
+			
+			pi.getTxtValIni().setText(vista.getCrudDepreciacion().getSeledRow(1));
+			pi.getTxtValRes().setText(vista.getCrudDepreciacion().getSeledRow(2));
+			pi.getTxtVidUtil().setText(vista.getCrudDepreciacion().getSeledRow(3));
+			
+			CalcularDepreciacion(pi.getModelRecta(), pi.getModelAcelerado());
+			
+			imprimir.registerPanel(pi, "pi");
+			
+			imprimir.setVisible(true);
+			
 		}
+	}
+	
+	
+	private void CalcularDepreciacion(DefaultTableModel modelRecta, DefaultTableModel modelAcelerado) {
+		
+		int valorInicial = Integer.parseInt(vista.getCrudDepreciacion().getSeledRow(1)); 
+		int valorRescate = Integer.parseInt(vista.getCrudDepreciacion().getSeledRow(2));
+		int vidaUtil = Integer.parseInt(vista.getCrudDepreciacion().getSeledRow(3));
+		
+
+			int[][] dataLineaRecta = calcularDepreciacionLineaRecta(valorInicial, valorRescate, vidaUtil);
+			int[][] dataAcelerado = calcularDepreciacionAcelerada(valorInicial, valorRescate, vidaUtil);
+			
+			vista.clearTables();
+			
+			
+			for(int i = 0; i<vidaUtil; i++) {
+				modelRecta.addRow(new  Object[] {
+						dataLineaRecta[i][0],
+						dataLineaRecta[i][1],
+						dataLineaRecta[i][2]+"%",
+						dataLineaRecta[i][3],
+						dataLineaRecta[i][4],
+						dataLineaRecta[i][5],
+				});
+				
+				modelAcelerado.addRow(new  Object[] {
+						dataAcelerado[i][0],
+						dataAcelerado[i][1],
+						dataAcelerado[i][2]+"%",
+						dataAcelerado[i][3],
+						dataAcelerado[i][4],
+						dataAcelerado[i][5],
+				});
+				
+			}
+		
+		
 	}
 	
 	
