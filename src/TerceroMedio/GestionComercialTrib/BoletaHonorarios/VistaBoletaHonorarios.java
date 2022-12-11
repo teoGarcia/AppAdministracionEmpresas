8e9a4dfd3f55dc4070b9152b4820b5af1c562b13
JampoolG <Jampoolgarcia@gmail.com>
2022-12-11 09:02:15 -0300

    reparacion de errores

--- src/TerceroMedio/GestionComercialTrib/BoletaHonorarios/VistaBoletaHonorarios.java
+++ src/TerceroMedio/GestionComercialTrib/BoletaHonorarios/VistaBoletaHonorarios.java
@@ -8,44 +8,16 @@ import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import TerceroMedio.GestionComercialTrib.Formulario22.ControlFormulario;
+import ui.AbrirDocumento.VistaDocumento;
 import ui.Buttons.TittleButton;
 
-public class VistaBoletaHonorarios extends JPanel {
+public class VistaBoletaHonorarios extends VistaDocumento {
 
-	private TittleButton stndrbtnVerDocumento;
-	//private ControlB
-	/**
-	 * Create the panel.
-	 */
 	public VistaBoletaHonorarios() {
-		
-	//	control = new ControlFormulario(this);
-		
-		setOpaque(false);
-		setBounds(0, 0, 774, 722);
-		setLayout(null);
-		
-		JLabel lblNewLabel = new JLabel("Formulario 22");
-		lblNewLabel.setForeground(Color.WHITE);
-		lblNewLabel.setFont(new Font("Dialog", Font.BOLD, 18));
-		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setBounds(0, 53, 774, 30);
-		add(lblNewLabel);
-		
-		stndrbtnVerDocumento = new TittleButton("Ver Documento");
-		stndrbtnVerDocumento.setBounds(194, 344, 400, 120);
-		//stndrbtnVerDocumento.addActionListener(control);
-		add(stndrbtnVerDocumento);
-
+		super("Boleta y Honorarios", "FORMATO BOLETA DE HONORARIOS.pdf");
 	}
 
-	public TittleButton getStndrbtnVerDocumento() {
-		return stndrbtnVerDocumento;
-	}
-
-	public void setStndrbtnVerDocumento(TittleButton stndrbtnVerDocumento) {
-		this.stndrbtnVerDocumento = stndrbtnVerDocumento;
-	}
+	
 
 
 }
